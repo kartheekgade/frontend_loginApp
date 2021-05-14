a07b1890429b76637aa2f8c4c4e2cc5c319362fd
kartheek.gade <gade.reddy@globallogic.com>
2021-05-14 10:27:44 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Register from './Register';
+import Login from './Login';
+
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+ 
+function App({navigation}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+        </Switch> 
+      </Router>
     </div>
+   );
+}
+
+const Home = () => {
+  return (
+  <div>
+    <h1>Home Page</h1>
+    <a href="login">Login</a><br/>
+    <a href="register">Register</a>
+  </div>
   );
 }
 
---
+++ src/Register.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Register extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+            email : '',
+			username : '',
+			password : ''
+		};
+	}
+
+	changeHandler = e => {
+		this.setState({ [e.target.name]: e.target.value});
+	}
+
+	submitHandler = e => {
+		e.preventDefault();
+		console.log(this.state);
+		axios.post("http://localhost:3000/user/register",this.state)
+		.then(response =>{
+			console.log(response);
+		}).catch(error => {
+			console.log(error)
+		})
+	}
+	
+	render() {
+		const {user,pass} = this.state
+		return(
+			<div>
+        		<h1>
+          			Register
+        		</h1>
+        		<form onSubmit={this.submitHandler}>
+                    email : <input onChange={this.changeHandler} value={user} name="email" placeholder="email" id= "emailBox" /><br />
+          			Username : <input onChange={this.changeHandler} value={user} name="username" placeholder="username" id= "usernameBox" /> <br /> 
+          			Password : <input onChange={this.changeHandler} value={pass} name="password" placeholder="password" id = "passwordBox" /> <br /> <br />
+          			<button type="submit">
+            			Register
+          			</button><br />
+        		</form>
+      		</div>
+		);
+	}
+}
+
+export default Register;
